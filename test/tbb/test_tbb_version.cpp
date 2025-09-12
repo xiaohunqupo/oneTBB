9d68318891200a1fd8628eab6ebb9e0113c05845
Alexandr-Konovalov <alexandr.konovalov@intel.com>
2025-09-12 12:13:15 +0200

    Fix missing brief covers for test_tbb_version.cpp (#1845)
    Co-authored-by: Konstantin Boyarinov <konstantin.boyarinov@intel.com>

--- test/tbb/test_tbb_version.cpp
+++ test/tbb/test_tbb_version.cpp
@@ -14,8 +14,9 @@
     limitations under the License.
 */
 
+//! Test for the availability of extensions
 //! \file test_tbb_version.cpp
-//! \brief Test for the availability of extensions
+//! \brief Test for [configuration.feature_macros] specification
 
 #include "tbb/version.h"
 // checking that inclusion of version.h is enough to get TBB_EXT_CUSTOM_ASSERTION_HANDLER
