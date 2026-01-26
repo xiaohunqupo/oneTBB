8b9ca4d80b24042e66022244c70dd9932a6de26c
Ganael Laplanche <ganael.laplanche@martymac.org>
2026-01-26 14:50:46 +0100

    FreeBSD: Add affinity support to tests (#1950)
    Co-authored-by: Konstantin Boyarinov <konstantin.boyarinov@intel.com>

--- test/common/utils_concurrency_limit.h
+++ test/common/utils_concurrency_limit.h
@@ -80,6 +80,12 @@ inline std::vector<thread_num_type> concurrency_range() {
 
 static int maxProcs = 0;
 
+#if __linux__
+using cpu_set_type = cpu_set_t;
+#elif __FreeBSD__
+using cpu_set_type = cpuset_t;
+#endif
+
 static int get_max_procs() {
     if (!maxProcs) {
 #if _WIN32||_WIN64
@@ -91,16 +97,16 @@ static int get_max_procs() {
                 ++nproc;
         }
         maxProcs = nproc;
-#elif __linux__
-        cpu_set_t mask;
+#elif __linux__ || __FreeBSD__
         int result = 0;
-        sched_getaffinity(0, sizeof(cpu_set_t), &mask);
+        cpu_set_type mask;
+        sched_getaffinity(0, sizeof(cpu_set_type), &mask);
         int nproc = sysconf(_SC_NPROCESSORS_ONLN);
         for (int i = 0; i < nproc; ++i) {
             if (CPU_ISSET(i, &mask)) ++result;
         }
         maxProcs = result;
-#else // FreeBSD
+#else
         maxProcs = sysconf(_SC_NPROCESSORS_ONLN);
 #endif
     }
@@ -328,9 +334,9 @@ private:
 
 std::vector<int> get_cpuset_indices() {
     std::vector<int> result;
-#if __linux__
-    cpu_set_t mask;
-    sched_getaffinity(0, sizeof(cpu_set_t), &mask);
+#if __linux__ || __FreeBSD__
+    cpu_set_type mask;
+    sched_getaffinity(0, sizeof(cpu_set_type), &mask);
     int nproc = sysconf(_SC_NPROCESSORS_ONLN);
     for (int i = 0; i < nproc; ++i) {
         if (CPU_ISSET(i, &mask)) {
@@ -339,7 +345,7 @@ std::vector<int> get_cpuset_indices() {
     }
     ASSERT(!result.empty(), nullptr);
 #else
-    // TODO: add affinity support for Windows and FreeBSD
+    // TODO: add affinity support for Windows
 #endif
     return result;
 }
