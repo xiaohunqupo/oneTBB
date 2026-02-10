7cb69a7e1d825b026865eb488229cb52d5b3ac24
Ilya Isaev <ilya.isaev@intel.com>
2026-02-10 12:49:36 +0100

    Use correct Intel copyright year for proxy.cpp (#1963)
    Signed-off-by: Isaev, Ilya <ilya.isaev@intel.com>

--- src/tbbmalloc_proxy/proxy.cpp
+++ src/tbbmalloc_proxy/proxy.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2005-2026 Intel Corporation
+    Copyright (c) 2005-2024 Intel Corporation
     Copyright (c) 2026 UXL Foundation Contributors
 
     Licensed under the Apache License, Version 2.0 (the "License");
