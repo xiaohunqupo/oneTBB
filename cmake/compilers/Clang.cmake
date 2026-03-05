97f44bc4284eb37fdd604818d54d971ad6f686a7
Ilya Isaev <ilya.isaev@intel.com>
2026-03-05 15:51:14 +0100

    Clang build on Windows: address comments from #1936 (#1992)
    Signed-off-by: Isaev, Ilya <ilya.isaev@intel.com>

--- cmake/compilers/Clang.cmake
+++ cmake/compilers/Clang.cmake
@@ -1,5 +1,5 @@
 # Copyright (c) 2020-2025 Intel Corporation
-# Copyright (c) 2025 UXL Foundation Contributors
+# Copyright (c) 2025-2026 UXL Foundation Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -71,18 +71,17 @@ endif()
 # Clang flags to prevent compiler from optimizing out security checks
 # Don't use -fPIC, -fstack-clash-protection, -fcf-protection on Windows (not supported by Clang with MSVC toolchain)
 # Also disable CRT security warnings on Windows (getenv, strncpy, etc. are deprecated but TBB uses them)
-if(WIN32)
-    set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS} -Wformat -Wformat-security -Werror=format-security -D_CRT_SECURE_NO_WARNINGS $<$<NOT:$<BOOL:${EMSCRIPTEN}>>:-fstack-protector-strong>)
-else()
-    set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS} -Wformat -Wformat-security -Werror=format-security -fPIC $<$<NOT:$<BOOL:${EMSCRIPTEN}>>:-fstack-protector-strong>)
-endif()
+set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS}
+    -Wformat -Wformat-security -Werror=format-security
+    $<$<PLATFORM_ID:Windows>:-D_CRT_SECURE_NO_WARNINGS>
+    $<$<NOT:$<PLATFORM_ID:Windows>>:-fPIC>
+    $<$<NOT:$<PLATFORM_ID:Emscripten>>:-fstack-protector-strong>)
 
 if (NOT APPLE AND NOT ANDROID_PLATFORM AND CMAKE_SYSTEM_PROCESSOR MATCHES "(AMD64|amd64|i.86|x86)" AND NOT WIN32)
-    set(TBB_LIB_COMPILE_FLAGS ${TBB_LIB_COMPILE_FLAGS} -fstack-clash-protection $<$<NOT:$<BOOL:${EMSCRIPTEN}>>:-fcf-protection=full>)
+    set(TBB_LIB_COMPILE_FLAGS ${TBB_LIB_COMPILE_FLAGS} -fstack-clash-protection $<$<NOT:$<PLATFORM_ID:Emscripten>>:-fcf-protection=full>)
 endif()
 
 # -z switch is not supported on MacOS and Windows
-# On Windows, lld-link doesn't support -z options (Linux-specific)
 if (NOT APPLE AND NOT WIN32)
     set(TBB_LIB_LINK_FLAGS ${TBB_LIB_LINK_FLAGS} -Wl,-z,relro,-z,now,-z,noexecstack)
 endif()
