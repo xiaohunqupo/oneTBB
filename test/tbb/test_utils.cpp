2fea40755cccad7eb8b0ee0c07ed0c4e75bf562c
Alexandr-Konovalov <alexandr.konovalov@intel.com>
2026-07-31 15:32:58 +0200

    Extend comments in test_utils.cpp (#2141)

--- test/tbb/test_utils.cpp
+++ test/tbb/test_utils.cpp
@@ -20,6 +20,8 @@
 //! \file test_utils.cpp
 //! \brief Test for [internal] functionality
 
+//! Testing alignment utility functions
+//! \brief \ref requirement
 TEST_CASE("Test alignment utility functions") {
     CHECK(tbb::detail::align_to_greater_or_equal<std::size_t>(0, 1) == 0);
     CHECK(tbb::detail::align_to_greater_or_equal<std::size_t>(1, 1) == 1);
